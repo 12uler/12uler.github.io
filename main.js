50b168aa64044a76c43d6dbbbc58ab5e2c5cf800
12uler <58787045+12uler@users.noreply.github.com>
2020-12-03 20:54:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 //https://cors-anywhere.herokuapp.com/https://a.4cdn.org/biz/10.json
-$.getJSON("https://cors-anywhere.herokuapp.com/https://a.4cdn.org/biz/10.json", function(getData) {
- 
-var 
-posDict = ["bull", "bullish", "moon", "miss out", "wtf was that", "gonna make it", "make it", "waiting room", "pump", "pump it", "whale", "linker", "no linker", "link marines", "rally", "comfy", "space mission"],
-negDict = ["pink wojack", "crash", "bobo", "red", "fud", "bear", "dump", "ahhhhhh", "ahhhhhhhh","aaaaaaaaahhhhhhhhhhhhh", "weak hands", "broke", "losses", "loss", "stinkers", "stinker", "buy the dip", "short", "reversal"],  
-rawComments = [],  
+//https://cors-anywhere.herokuapp.com/https://a.4cdn.org/biz/10.json
+$(document).ready(function() {
+var newPosEntry = $('#posUserKeywords').text(),
+    newNegEntry = $('#negUserKeywords').text();
+  
+var posDict = ["bull", "bullish", "moon", "miss out", "wtf was that", "gonna make it", "make it", "waiting room", "pump", "pump it", "whale", "linker", "no linker", "link marines", "rally", "comfy", "space mission"],
+    
+  negDict = ["pink wojack", "crash", "bobo", "red", "fud", "bear", "dump", "ahhhhhh", "ahhhhhhhh","aaaaaaaaahhhhhhhhhhhhh", "weak hands", "broke", "losses", "loss", "stinkers", "stinker", "buy the dip", "short", "reversal"]; 
+
+  function getComments(){
+$.getJSON("https://raw.githubusercontent.com/12uler/12uler.github.io/master/biz.json",
+          
+function(getData){
+var rawComments = [],  
 ctx = document.getElementById('graph').getContext('2d');
   
 for(let x = 0; x < 14; x++) {
@@ -13,10 +21,9 @@ for(let x = 0; x < 14; x++) {
 rawComments.push(getData.threads[x].posts[i].com);
      }
    }
-  
+ 
 //raw comments refinement to lowercase
 var comments = rawComments.join('|').toLowerCase().split('|');
-  
   
 //filter functions
 function checkPos(val){
@@ -26,18 +33,18 @@ function checkPos(val){
 function checkNeg(value){
   return negDict.every(function(z){return value.indexOf(z) == -1 });
 };
-  
+
 //filter comments
-var posComments = comments.filter(checkPos);
-var negComments = comments.filter(checkNeg);
-var posData = posComments.length;
-var negData = negComments.length;
+var posComments = comments.filter(checkPos),
+ negComments = comments.filter(checkNeg),
+ posData = posComments.length,
+ negData = negComments.length;
 console.log(posComments.length);
 console.log(negComments.length);
-//document.getElementById('comments').innerHTML = negComments;
  
+//document.getElementById('comments').innerHTML = negComments;
  //build the graph
-var sentimentChart = new Chart(ctx, {
+ var chart = new Chart(ctx, {
   type: 'bar',
   responsive: true,
   barValueSpacing: 2,
@@ -63,17 +70,54 @@ var sentimentChart = new Chart(ctx, {
    }
 }); 
 
-      //Positive or Negative? 
-    if(posData>negData){
-     return $('#result').text("Positive Sentiment"),
-     $('#result').css("background-color", "green");
-    } else if (negData > posData){
-     return $('#result').text("Negative Sentiment"),
-      $('#result').css("background-color", "red");
-    } else {
-     return $('#result').text("No Change"),
-      $('#result').css("background-color", "gray");;
-    }
-   
+    //Positive or Negative? 
+
+if(posData>negData){
+    return $('#result').text("Positive Sentiment"),
+    $('#result').css("background-color", "green");
+  } else if (negData > posData){
+    return $('#result').text("Negative Sentiment"),
+     $('#result').css("background-color", "red");
+  } else {
+    return $('#result').text("No Change"),
+     $('#result').css("background-color", "gray");;
+  }  
  
-}); //getData end 
+});  //JSON sort function end 
+    
+$('#posDict').text(posDict);
+$('#negDict').text(negDict);    
+ console.log(posDict);
+ console.log(negDict);
+} 
+ 
+$('#updateButton').on('click', getComments); //Click to reload button   
+
+$('#enterPosKey').on('click', function(){
+    let newWords = prompt("Input Keywords. Ex: yay, great, pump");
+    if(newWords != null){
+      newWords.toString();
+      let tempArr = newWords.split(', ');
+      for(let i=0; i<tempArr.length; i++){
+      posDict.push(tempArr[i]);
+      }
+    }
+    console.log(posDict);
+  });
+  
+ $('#enterNegKey').on('click', function(){
+    let newWords = prompt("Input Keywords. Ex: bad, drop, red");
+    if(newWords != null){
+      newWords.toString();
+      let tempArr = newWords.split(', ');
+      for(let i=0; i<tempArr.length; i++){
+      negDict.push(tempArr[i]);
+      }
+    }
+    console.log(negDict);
+  });
+  
+ getComments(); 
+  
+}); //doc on ready func end
+
